903b7ad75c1b1b093af347992e9ada7c3aa72528
dhini <ramdhininovita0811@gmail.com>
2025-07-18 10:58:08 +0700

    Revert "Fix copyplugin"
    This reverts commit e3a1dc4d9ab46db2ff52680e9a8cc63251e015d8.

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -1,7 +1,6 @@
 
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -28,13 +27,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-        template: './src/index.html',
-        filename: 'index.html',
+      template: './src/index.html',
     }),
-    new CopyPlugin({ 
-      patterns: [
-        { from: 'src/_redirects', to: '_redirects' }, 
-      ],
-    }),
-],
+  ],
 };
