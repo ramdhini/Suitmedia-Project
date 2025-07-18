e3a1dc4d9ab46db2ff52680e9a8cc63251e015d8
dhini <ramdhininovita0811@gmail.com>
2025-07-18 10:35:56 +0700

    Fix copyplugin

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -1,6 +1,7 @@
 
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -27,7 +28,13 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html',
+        template: './src/index.html',
+        filename: 'index.html',
     }),
-  ],
+    new CopyPlugin({ 
+      patterns: [
+        { from: 'src/_redirects', to: '_redirects' }, 
+      ],
+    }),
+],
 };
